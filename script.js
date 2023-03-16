db5557181c5d318895cc621dc75d376cdf5bc077
Mgamboa <marianellag400@gmail.com>
2023-03-16 14:29:06 -0500

    saving progress

--- script.js
+++ script.js
@@ -34,3 +34,31 @@ resetBtn.addEventListener('click', function () {
     // telling the system specifically to set to 00
 });
 
+function stopWatch() {
+    //from line 13, where its first introduced.
+    if (timer) {
+        count++;
+        //if timer is truthee, count up.
+        if (count == 100) {
+            // mozilla def. :The equality ( == ) operator checks whether its two operands are equal, returning a Boolean result.
+            //100 seconds takes to a minute
+            second++; //increases seconds
+            count = 0;
+        }
+        if (second == 60) {
+            //60 seconds to a minute
+            minute++; //increase minutes
+            second = 0;
+        }
+        if (minute == 60) {
+            //60 min. to hour
+            hour++;
+            minute = 0;
+            second = 0;
+            //all these turn to 0, once every 60 hits
+        }
+
+
+
+    }
+}
